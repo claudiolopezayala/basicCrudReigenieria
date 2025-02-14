d3e315027a75451b45b18ab45327763d4d44c295
claudiwicode <claudio.wicode@gmail.com>
2025-02-13 21:56:19 -0600

    add port as env

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import { PORT } from "@utils/dotenv"
+import express, {Request, Response} from "express"
+import { StatusCodes } from "http-status-codes"
+
+const app = express()
+
+app.get("/",(req:Request, res:Response)=>{
+  res.status(StatusCodes.OK).send()
+})
+
+app.listen(PORT,()=>{
+  console.log(`Listening on:\nhttp://localhost:${PORT}/`)
+})
