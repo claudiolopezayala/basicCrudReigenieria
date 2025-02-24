a7c418976ff4a59ada887d11bd8dd1cb04b86192
claudiwicode <claudio.wicode@gmail.com>
2025-02-24 14:39:40 -0600

    add cors

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,19 @@
-import { db } from "./db/connection.db"
 import { PORT } from "./utils/dotenv"
 import express, {Request, Response} from "express"
-import { StatusCodes } from "http-status-codes"
-import {testTable} from "./db/schemas/testSchema"
 import { router } from "./routes/app.routes"
+import cors from "cors";
 
 const app = express()
 
 app.use(express.json());
+
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
 
 app.use("/", router);
 
